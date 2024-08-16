3aac184237d1b503f248d8e4d367892ee8fc5172
Alexey Tyapkin <Marveleks@gmail.com>
2024-08-16 16:38:04 +0300

    Начал работать над имплементацией интерфейсов

--- Routing/Routing/Router.cs
+++ Routing/Routing/Router.cs
@@ -2,22 +2,53 @@
 
 namespace Routing.Routing
 {
-
     public class Router : IRouter
     {
-        public void RegisetRoute<T1>(string template, Action<T1> action)
+        private Dictionary<string, RouteData> routingTable = new Dictionary<string, RouteData>();
+
+        public void RegisterRoute(string template, Action action)
         {
-            throw new NotImplementedException();
+            var routeData = new RouteData(template, action);
+            routingTable.Add(template, routeData);
         }
 
-        public void RegisetRoute<T1, T2>(string template, Action<T1, T2> action)
+        public void RegisterRoute<T1>(string template, Action<T1> action)
         {
-            throw new NotImplementedException();
+            var routeData = new RouteData(template, action);
+            routingTable.Add(template, routeData);
+        }
+
+        public void RegisterRoute<T1, T2>(string template, Action<T1, T2> action)
+        {
+            var routeData = new RouteData(template, action);
+            routingTable.Add(template, routeData);
         }
 
         public void Route(string route)
         {
-            throw new NotImplementedException();
+            if (routingTable.TryGetValue(route, out var result))
+            {
+                result.Method.DynamicInvoke();
+            }
+        }
+
+
+
+        private class RouteData
+        {
+            public readonly Delegate Method;
+            /// <summary>
+            ///     ArgName - Type
+            /// </summary>
+            public readonly Dictionary<string, string> Arguments;
+            public readonly string StaticPath;
+
+
+
+            public RouteData(string path, Delegate method)
+            {
+                Method = method;
+            }
         }
     }
 }
