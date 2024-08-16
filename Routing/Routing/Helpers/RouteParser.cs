4c75cd1109292b87920a67bc2e10be18c98560c0
Alexey Tyapkin <Marveleks@gmail.com>
2024-08-16 16:37:36 +0300

    Создал парсер аргументов из строки регистрации

---
+++ Routing/Routing/Helpers/RouteParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace STCTesting.Routing.Helpers
+{
+    public class RouteParser
+    {
+        public static Dictionary<string, string> GetRegistratorArguments(string path)
+        {
+            var arguments = new Dictionary<string, string>();
+
+            var matches = Regex.Matches(path, "{\\w*:\\w*}");
+
+            if (matches == null)
+            {
+                return arguments;
+            }
+
+            foreach (Match match in matches)
+            {
+                if (!match.Success)
+                    continue;
+
+                var argument = match.Value.Split(":");
+                //Наименование переменной, значение
+                arguments.Add(argument[0], argument[1]);
+            }
+
+            return arguments;
+        }
+    }
+
+
+}
+    
