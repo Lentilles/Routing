19ffb299d227653e674e4286b74734ce37442fd4
Alexey Tyapkin <Marveleks@gmail.com>
2024-08-16 16:37:10 +0300

    Добавил примеры регистрации и вызовов

--- Routing/Program.cs
+++ Routing/Program.cs
@@ -4,6 +4,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        Router router = new Router();
 
+        router.RegisterRoute("/foo/bar/", () => { Console.WriteLine("WithoutArguments"); });
+        router.RegisterRoute<int>("/foo/bar/{a:int}/", (a) => { Console.WriteLine(a); });
+        router.RegisterRoute<int, int>("/foo/bar/{a:int}/{b:int}/", (a, b) => { Console.WriteLine($"{a} | {b}"); });
+
+        router.Route("/foo/bar/");
+        router.Route("/foo/bar/{a:int}/");
+        router.Route("/foo/bar/{a:int}/{b:int}/");
     }
 }
