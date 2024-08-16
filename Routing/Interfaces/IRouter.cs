f14cad41845b97b7c2e1857d0690269ceb43600c
Alexey <Marveleks@gmail.com>
2024-08-16 21:01:22 +0300

    Переделал интерфейс, теперь он более  абстрактный

--- Routing/Interfaces/IRouter.cs
+++ Routing/Interfaces/IRouter.cs
@@ -8,10 +8,22 @@ namespace Routing.Interfaces
 {
     public interface IRouter
     {
-        void RegisterRoute(string template, Action action);
-        void RegisterRoute<T1>(string template, Action<T1> action);
-        void RegisterRoute<T1, T2>(string template, Action<T1, T2> action);
 
+        /*
+         * Изменил контракты интерфейса на более универсальный параметр
+         * За счет этого не нужно будет создавать новые контракты в случае необходимости
+         * С любым количеством параметров
+         * Но тогда у нас меняется объявление аргументов с (a, b) на (T a, T b)
+         */
+
+        /// <summary>
+        /// Регистрирует маршрут формата /foo/bar/{name:type}/{name2:type}/
+        /// /foo/bar - статическая часть.
+        /// Где {name:type} - динамическая часть.
+        /// </summary>
+        /// <param name="template">Шаблон маршрута</param>
+        /// <param name="action">Делегат с любым набором аргументов</param>
+        void RegisterRoute(string template, Delegate action);
         void Route(string route);
     }
 }
